06a9b23e9c7e05c68adfdcbbf572117acf25f4d4
ysbdfb <amk1001896@student.hamk.fi>
2025-09-14 23:17:23 +0300

    Step 2.2

--- Lucky7/src/App.java
+++ Lucky7/src/App.java
@@ -2,33 +2,48 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         Random random = new Random();
+        int money = 5;
+        while(money > 0) {
         int RandomNumber1 = random.nextInt(10) + 1;
         int RandomNumber2 = random.nextInt(10) + 1;
         int RandomNumber3 = random.nextInt(10) + 1;
         System.out.println(RandomNumber1 + " " + RandomNumber2 + " " + RandomNumber3);
         if(RandomNumber1 == 7 && RandomNumber2 == 7 && RandomNumber3 == 7) {
-            System.out.println("You won");
+            money += 9;
+            System.out.println(" You won 10€.\n Your balance is " + money + " €.\n");
         }
         else if(RandomNumber1 == 7 && RandomNumber2 == 7) {
-            System.out.println("You won");
+            money += 4;
+            System.out.println(" You won 5€.\n Your balance is " + money + " €.\n");
         }
         else if(RandomNumber1 == 7 && RandomNumber3 == 7) {
-            System.out.println("You won");
+            money += 4;
+            System.out.println(" You won 5€.\n Your balance is " + money + " €.\n");
         }
         else if(RandomNumber2 == 7 && RandomNumber3 == 7) {
-            System.out.println("You won");
+            money += 4;
+            System.out.println(" You won 5€.\n Your balance is " + money + " €.\n");
         }
         else if(RandomNumber1 == 7) {
-            System.out.println("You won");
+            money += 2;
+            System.out.println(" You won 3€.\n Your balance is " + money + " €.\n");
         }
         else if(RandomNumber2 == 7) {
-            System.out.println("You won");
+            money += 2;
+            System.out.println(" You won 3€.\n Your balance is " + money + " €.\n");
         }
         else if(RandomNumber3 == 7) {
-            System.out.println("You won");
+            money += 2;
+            System.out.println(" You won 3€.\n Your balance is " + money + " €.\n");
         }
         else {
-            System.out.println("You lost");
+            money--;
+            System.out.println(" You lost this game.\n Dont't give up!\n Your balance is " + money + " €.\n");
+        }
+        if(money == 0) {
+            System.out.println(" You lost everything.\n Game is over");
+            break;
+        }
         }
     }
 }
