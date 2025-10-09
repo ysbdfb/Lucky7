e67197d6ac72afa39b2a09ef4048a39e2af5ea5a
ysbdfb <amk1001896@student.hamk.fi>
2025-10-09 16:15:25 +0300

    Update App.java

--- Lucky7/src/App.java
+++ Lucky7/src/App.java
@@ -1,8 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Random random = new Random();
-        int money = 5;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ready to spin? Place your bet!");
+        int money = Integer.parseInt(in.nextLine());
+        System.out.println("Let's get started!");
+        String answer;
         while(money > 0) {
         int RandomNumber1 = random.nextInt(10) + 1;
         int RandomNumber2 = random.nextInt(10) + 1;
@@ -10,40 +15,51 @@ public class App {
         System.out.println(RandomNumber1 + " " + RandomNumber2 + " " + RandomNumber3);
         if(RandomNumber1 == 7 && RandomNumber2 == 7 && RandomNumber3 == 7) {
             money += 9;
-            System.out.println(" You won 10€.\n Your balance is " + money + " €.\n");
+            System.out.println("You won 10$.\nYour balance is " + money + "$.\n");
         }
         else if(RandomNumber1 == 7 && RandomNumber2 == 7) {
             money += 4;
-            System.out.println(" You won 5€.\n Your balance is " + money + " €.\n");
+            System.out.println("You won 5$.\nYour balance is " + money + "$.\n");
         }
         else if(RandomNumber1 == 7 && RandomNumber3 == 7) {
             money += 4;
-            System.out.println(" You won 5€.\n Your balance is " + money + " €.\n");
+            System.out.println("You won 5$.\nYour balance is " + money + "$.\n");
         }
         else if(RandomNumber2 == 7 && RandomNumber3 == 7) {
             money += 4;
-            System.out.println(" You won 5€.\n Your balance is " + money + " €.\n");
+            System.out.println("You won 5$.\nYour balance is " + money + "$.\n");
         }
         else if(RandomNumber1 == 7) {
             money += 2;
-            System.out.println(" You won 3€.\n Your balance is " + money + " €.\n");
+            System.out.println("You won 3$.\nYour balance is " + money + "$.\n");
         }
         else if(RandomNumber2 == 7) {
             money += 2;
-            System.out.println(" You won 3€.\n Your balance is " + money + " €.\n");
+            System.out.println("You won 3$.\nYour balance is " + money + "$.\n");
         }
         else if(RandomNumber3 == 7) {
             money += 2;
-            System.out.println(" You won 3€.\n Your balance is " + money + " €.\n");
+            System.out.println("You won 3$.\nYour balance is " + money + "$.\n");
         }
-        else {
+        else if(RandomNumber1 != 7 && RandomNumber2 != 7 && RandomNumber3 != 7 && money > 0) {
             money--;
-            System.out.println(" You lost this game.\n Dont't give up!\n Your balance is " + money + " €.\n");
+            System.out.println("You lost this game.\nDont't give up!\nYour balance is " + money + "$.\n");
         }
+        
         if(money == 0) {
-            System.out.println(" You lost everything.\n Game is over");
+            System.out.println("You lost everything.\nGame is over.");
+            break;
+        }
+        System.out.println("Would you like to play again?\nType: yes/no\n");
+        answer = in.nextLine();
+        if(answer.equalsIgnoreCase("yes")) {
+            System.out.println("Again!");
+        }
+        else if(answer.equalsIgnoreCase("no")) {
+            System.out.println("Thanks for playing!");
             break;
         }
         }
+        in.close();
     }
 }
